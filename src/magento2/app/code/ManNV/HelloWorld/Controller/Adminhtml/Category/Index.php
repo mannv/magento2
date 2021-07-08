cde75d98afd8e2852a3bad6f703bf05ed8bc4a8d
mannv <anhmantk@gmail.com>
2021-07-09 05:10:33 +0700

    add module hello world

---
+++ src/magento2/app/code/ManNV/HelloWorld/Controller/Adminhtml/Category/Index.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace ManNV\HelloWorld\Controller\Adminhtml\Category;
+use Magento\Framework\View\Result\PageFactory;
+use Magento\Backend\App\Action;
+
+class Index extends \Magento\Backend\App\Action
+{
+    protected $_pageFactory;
+
+    public function __construct(Action\Context $context, PageFactory $pageFactory)
+    {
+        $this->_pageFactory = $pageFactory;
+        return parent::__construct($context);
+    }
+
+    public function execute()
+    {
+        return $this->_pageFactory->create();
+    }
+}
